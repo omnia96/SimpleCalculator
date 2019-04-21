95fdfa422eaca11b4a34b0f39a3c0d7b110bc725
omnia96 <dongjun1997@outlook.com>
2019-04-21 21:04:44 +0800

    基本页面绘制

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,21 @@ const app = getApp()
 
 Page({
   data: {
+    number:""
   },
   onLoad: function () {
   },
+  handleKeyTap:function(e){
+    console.log(e.target.id)
+    var that = this
+    var number = that.data.number
+    that.setData({
+      number:number+e.target.id
+    })
+  },
+  handleCalculatingKeyTap:function(e){
+    console.log(e.target.id)
+    var that = this
+    var number = parseFloat(that.data.number)
+  }
 })
