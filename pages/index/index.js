432c1cef663f77db26d1711f3a319eb1de427866
omnia96 <dongjun1997@outlook.com>
2019-04-22 18:02:32 +0800

    修改显示

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,38 +4,129 @@ const app = getApp()
 
 Page({
   data: {
-    number:"",
-    number_1:Number
+    result: 0,
+    number: "",
+    number1: Number,
+    number2: Number,
+    CalculatingKey: "",
+    notes:""
   },
   onLoad: function () {
+
   },
-  handleKeyTap:function(e){
+  handleKeyTap: function (e) {
     console.log(e.target.id)
     var that = this
     var number = that.data.number
+    var notes = that.data.notes
+    console.log(number)
     that.setData({
-      number:number+e.target.id
+      number: number + e.target.id,
+      result: number + e.target.id,
+      notes:notes + e.target.id
     })
   },
-  handleCalculatingKeyTap:function(e){
+  handleCalculatingKeyTap: function (e) {
     var that = this
     console.log(e.target.id)
-    if(e.target.id == "+"){
-      var number = parseFloat(that.data.number)
+    var number = that.data.number
+    if (number != "") {
+      switch (e.target.id) {
+        case "+":
+          that.setData({
+            number1: number,
+            number: "",
+            CalculatingKey: "+",
+          })
+          break;
+        case "-":
+          that.setData({
+            number1: number,
+            number: "",
+            CalculatingKey: "-"
+          })
+          break;
+        case "×":
+          that.setData({
+            number1: number,
+            number: "",
+            CalculatingKey: "×"
+          })
+          break;
+        case "÷":
+          that.setData({
+            number1: number,
+            number: "",
+            CalculatingKey: "÷"
+          })
+          break;
+        default:
+          break;
+      }
       that.setData({
-        number_1:number,
-        number:""
+        notes:that.data.notes+that.data.CalculatingKey
+      })
+    }else{
+      that.setData({
+        CalculatingKey:e.target.id,
+        notes:number+that.data.CalculatingKey
       })
     }
-    if(e.target.id == "="){
-      var result = that.Addition(that.data.number_1,parseFloat(that.data.number))
-      that.setData({
-        number:result
-      })
+    if (e.target.id == "±") {
+      if (that.data.number != "") {
+        var number = parseFloat(that.data.number)
+        that.setData({
+          number: -(number)
+        })
+      } else {
+        var number = parseFloat(that.data.number1)
+        that.setData({
+          number1: -(number)
+        })
+      }
+
     }
-    
+
+    if (e.target.id == "=") {
+      if (that.data.number != "") {
+        that.setData({
+          number2: that.data.number
+        })
+      }
+      var result = 0;
+      switch (that.data.CalculatingKey) {
+        case "+":
+          result = that.Addition(that.data.number1, that.data.number2)
+          break;
+        case "-":
+          result = that.Subtraction(that.data.number1, that.data.number2)
+          break;
+        case "×":
+          result = parseFloat(that.data.number1) * parseFloat(that.data.number2)
+          break;
+        case "÷":
+          result = parseFloat(that.data.number1) / parseFloat(that.data.number2)
+          break;
+
+
+        default:
+          break;
+      }
+      that.setData({
+        number: "",
+        result: result,
+        number1: result,
+        notes:""
+      })
+      
+    }
+
   },
-  Addition:function(number1,number2) {
-    return number1+number2
+  Addition: function (number1, number2) {
+
+    return parseFloat(number1) + parseFloat(number2)
+  },
+  Subtraction: function (number1, number2) {
+    return parseFloat(number1) - parseFloat(number2)
   }
 })
